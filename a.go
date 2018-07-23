c9dbbf6c812b2991d2e4b1336fb31863a4de9985
Matt King <kingy895@gmail.com>
2018-07-23 14:21:11 -0700

    WIP

---
+++ a.go
@@ -0,0 +1,89 @@
+package jump
+
+func aaaaaaaaaa() {
+	bbbbbbbbbb()
+	cccccccccc()
+}
+
+/*
+ * does something.
+ */
+func eeeeeeeeeeeeee() {
+	bbbbbbbbbb()
+	cccccccccc()
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+func AAAAAAAAAA() {
+	eeeeeeeeeeeeee()
+	BBBBBBBBBB()
+	CCCCCCCCCC()
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
---
+++ b.go
@@ -0,0 +1,75 @@
+package jump
+
+func bbbbbbbbbb() {
+	aaaaaaaaaa()
+	cccccccccc()
+}
+
+func eeeeee() {
+	aaaaaaaaaa()
+	cccccccccc()
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// --
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// --
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// --
+//
+//
+//
+//
+// --
+//
+//
+
+func BBBBBBBBBB() {
+	AAAAAAAAAA()
+	CCCCCCCCCC()
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
